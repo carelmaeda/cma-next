8e3791bdf5ef957ddc6b743180b068c0a42585fa
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-02-26 18:25:33 -0500

    fix: about page

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -5,24 +5,22 @@ export default function About() {
     <section className="section-wrapper">
         <h2 className="text-center mb-4">About Me</h2>
         <div className="about-wrapper d-md-flex">
-           <Image src="/images/profile.jpeg" alt="profile"  width={200}  height={200}   />
-        <div className="about-text">
-        <p>
-            Sou <strong>Designer Gráfico</strong> e <strong>Social Media</strong>, especializado na criação de conteúdo, 
-            planejamento estratégico e edição de materiais visuais para diferentes plataformas. Com experiência em diversos setores, 
-            desenvolvo campanhas criativas que fortalecem a conexão entre marcas e público.
-        </p>
-        <p>
-            Minha formação em <strong>Design</strong> pelo Istituto Europeo di Design, aliada a especializações em 
-            <strong> Marketing Digital</strong> e <strong>UX Design</strong>, me permite unir criatividade e estratégia para entregar 
-            soluções eficazes e alinhadas às necessidades de cada cliente. Estou sempre em busca de inovação para destacar negócios no 
-            mercado e aprimorar sua comunicação.
-        </p>
-        <p>
-            Vamos transformar sua ideia em algo impactante?
-        </p>
-        <button className="btn btn-primary">Marque Sua Consulta</button>
-        </div>
+           <Image src="/images/profile.jpg" alt="profile"  width={200}  height={200}   />
+           <div className="about-text">
+            <p>
+              I am a <strong>Product Design Manager</strong> with over 5 years of experience in the industry.  
+              My work primarily involves leading projects for client UX and Service Design teams, focusing on 
+              creating <strong>user-friendly platforms</strong>, designing <strong>user research studies</strong>, and applying 
+              <strong> Design Thinking Methodologies</strong> to uncover valuable user insights. I collaborate closely with teams 
+              to translate these insights into impactful design solutions while also managing <strong>project workflows</strong> 
+              and contributing to <strong>front-end development</strong> to ensure seamless execution.
+            </p>
+            <p>
+              As an alumnus of <strong>Parsons School of Design</strong> and <strong>University of Toronto</strong>, I specialize in 
+              designing platforms that are scalable, responsive, and, most importantly, accessible to all users.
+            </p>
+          </div>
+
 
         </div>
 
