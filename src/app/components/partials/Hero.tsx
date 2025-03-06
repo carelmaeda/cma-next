5fafb1e102b3b565e860479ce35afd747a9c1845
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-03-05 22:45:22 -0500

    fix: hero text

--- src/app/components/partials/Hero.tsx
+++ src/app/components/partials/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
           <div className="">
             <h1 className="display-5">Hi! My name is <span className='hero-text'>Carel.</span> </h1>
             <p className="lead">
-            UX UI blablabla
+Crafting seamless UX/UI designs that put users first and brands ahead.
             </p>
             </div>
     </section>
