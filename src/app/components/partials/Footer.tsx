5052f6ef577dc291ff5714178f3a98a3457040d1
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-03-16 18:07:18 -0400

    fix: footer font

--- src/app/components/partials/Footer.tsx
+++ src/app/components/partials/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
       <div className="container text-muted">
           <h6>Thank you for your interest in my work!</h6>
           <div>
-          <p className="m-0">This portfolio was <a href="https://www.figma.com/design/vINdCUCFAALYVx9bto0Nq5/CMA-Portfolio?node-id=75-2169&t=SgGC9GVQ7sCtWpMG-1">designed</a> in Figma and <a href="https://github.com/carelmaeda/cma-next.git">developed</a> with Next.js, TypeScript, and Bootstrap.</p>
+          <small className="d-block">This portfolio was <a target="_blank" href="https://www.figma.com/design/vINdCUCFAALYVx9bto0Nq5/CMA-Portfolio?node-id=75-2169&t=SgGC9GVQ7sCtWpMG-1">designed</a> in Figma and <a target="_blank" href="https://github.com/carelmaeda/cma-next.git">developed</a> with Next.js, TypeScript, and Bootstrap.</small>
           <small>&copy; {year ? year : ""} Carel Maeda. All rights reserved.</small>
           </div>
       </div>
