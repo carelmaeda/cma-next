517793162917849486922e040b7085553c0b7e44
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-03-18 11:05:13 -0400

    feat: hero btn

--- src/app/components/partials/Hero.tsx
+++ src/app/components/partials/Hero.tsx
@@ -13,6 +13,7 @@ export default function Hero() {
             <p className="lead">
                 Crafting seamless UX/UI designs that put users first and brands ahead.
             </p>
+            <a href="#contact" className="btn btn-dark">Work with Me</a>
             </div>
     </section>
   );
