543c5c122334a3a030aad148059f42c7b1cbe606
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-03-19 10:25:08 -0400

    fix: about text

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -19,12 +19,11 @@ export default function About() {
           height={200}
         />
         <div className="about-text">
-          <p>
-            I am a <strong>Product Design Manager</strong> with over 5 years of experience in the industry. My work primarily involves leading projects for client UX and Service Design teams, focusing on creating <strong>user-friendly platforms</strong>, designing <strong>user research studies</strong>, and applying <strong>Design Thinking Methodologies</strong> to uncover valuable user insights. I collaborate closely with teams to translate these insights into impactful design solutions while also managing <strong>project workflows</strong> and contributing to <strong>front-end development</strong> to ensure seamless execution.
-          </p>
-          <p>
-            As an alumnus of <strong>Parsons School of Design</strong> and <strong>University of Toronto</strong>, I specialize in designing platforms that are scalable, responsive, and, most importantly, accessible to all users.
-          </p>
+         <p>I am a <strong>Product Design Manager</strong> with 6+ years of experience in <strong>UX and UI design</strong>.</p>
+         <p>I create user-friendly platforms, conduct user research, and use <strong>Design Thinking</strong> to find valuable insights. Working closely with teams, I turn these insights into effective designs while managing projects and contributing to <strong>front-end development</strong> for a smooth user experience.</p>
+        <p>As an alumnus of <strong>Parsons School of Design</strong> and <strong>University of Toronto</strong>, I specialize in designing platforms that are scalable, responsive, and, most importantly, accessible to all users.</p>
+        <p>Born and raised in Brasil, I am currently based in Toronto, Canada. In my free time, you can find me exploring the city with a camera around my neck. &#128247; </p>
+
           {/* Resume Button */}
           <button
             className="btn btn-light d-flex gap-2 align-items-center"
